870e95bb8d576913da880b6d25b63672dec79467
sarah105 <sara.said995@yahoo.com>
2021-09-24 21:37:17 +0200

    add IRepository interface

---
+++ Game_Models/Repositories/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_Models.Repositories
+{
+    public interface IRepository<TEntity>
+    {
+        IList<TEntity> List();
+        void Add(TEntity entity);
+        void Delete(int id);
+        TEntity Find(int id);
+        void Update(TEntity entity);
+    }
+}

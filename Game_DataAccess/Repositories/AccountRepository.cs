c397f3de3b0e01f0f345655605ceab22115c33db
sarah105 <sara.said995@yahoo.com>
2021-09-24 22:13:37 +0200

    add accountRepository

---
+++ Game_DataAccess/Repositories/AccountRepository.cs
@@ -0,0 +1,52 @@
+﻿using Game_DataAccess.Data;
+using Game_Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_DataAccess.Repositories
+{
+    class AccountRepository : IRepository<Account>
+    {
+        private readonly GameDbContext db;
+
+        public AccountRepository(GameDbContext db)
+        {
+            this.db = db;
+        }
+        public bool Add(Account account)
+        {
+            db.Accounts.Add(account);
+            return Save();
+        }
+
+        public bool Delete(Account account)
+        {
+            db.Accounts.Remove(account);
+            return Save();
+        }
+
+        public Account Find(int id)
+        {
+            var account = db.Accounts.SingleOrDefault(item => item.Id == id);
+            return account;
+        }
+
+        public IList<Account> List()
+        {
+            return db.Accounts.ToList();
+        }
+
+        public bool Update(Account entity)
+        {
+            db.Accounts.Update(entity);
+            return Save();
+        }
+        private bool Save()
+        {
+            return db.SaveChanges() >= 0;
+        }
+    }
+}
--- Game_DataAccess/Repositories/IRepository.cs
+++ Game_DataAccess/Repositories/IRepository.cs
@@ -4,14 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Game_Models.Repositories
+namespace Game_DataAccess.Repositories
 {
     public interface IRepository<TEntity>
     {
         IList<TEntity> List();
-        void Add(TEntity entity);
-        void Delete(int id);
+        bool Add(TEntity entity);
+        bool Delete(TEntity entity);
         TEntity Find(int id);
-        void Update(TEntity entity);
+        bool Update(TEntity entity);
     }
 }
